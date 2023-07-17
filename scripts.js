d84349e5f8bad0aeec8756561b671d817f5c9c87
Lea <leandrotosunian@hotmail.com>
2023-07-17 04:04:08 -0300

    test scroll

--- scripts.js
+++ scripts.js
@@ -1,21 +1,47 @@
-document.addEventListener("DOMContentLoaded", e => {
-    scrollFunction()
+const btnBarberia = document.querySelector("#btnBarberia")
+const btnClub = document.querySelector("#btnClub")
+const btnTienda = document.querySelector("#btnTienda")
+const btnAsociate = document.querySelector("#btnAsociate")
+const btnContacto = document.querySelector("#btnContacto")
+
+window.addEventListener("scroll", e => {
+    console.log(document.documentElement.scrollTop)
 })
 
-const scrollFunction = () => {
-    const btnBarberia = document.querySelector("#btnBarberia")
-    
-    window.addEventListener("scroll", e => {
-        console.log(document.documentElement.scrollTop)
+btnBarberia.addEventListener("click", e => {
+    window.scrollTo({
+        behavior: "smooth",
+        top: 4470
+    })
+})
 
+btnAsociate.addEventListener("click", e => {
+    window.scrollTo({
+        behavior: "smooth",
+        top: 2110
     })
-    
-    document.addEventListener("click", e => {
-        window.scrollTo({
-            behavior: "smooth",
-            top: 4670
-        })
+})
+
+btnClub.addEventListener("click", e => {
+    window.scrollTo({
+        behavior: "smooth",
+        top: 1098
     })
-}
+})
+
+btnContacto.addEventListener("click", e => {
+    window.scrollTo({
+        behavior: "smooth",
+        top: 4470
+    })
+})
+
+btnTienda.addEventListener("click", e => {
+    window.scrollTo({
+        behavior: "smooth",
+        top: 4470
+    })
+})
+
 
 
