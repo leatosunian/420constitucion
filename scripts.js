0d4d48ee316b8ba2d96cac0a66f9332373091758
Lea <leandrotosunian@hotmail.com>
2023-07-17 03:54:16 -0300

    scroll test

---
+++ scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", e => {
+    scrollFunction()
+})
+
+const scrollFunction = () => {
+    const btnBarberia = document.querySelector("#btnBarberia")
+    
+    window.addEventListener("scroll", e => {
+        console.log(document.documentElement.scrollTop)
+
+    })
+    
+    document.addEventListener("click", e => {
+        window.scrollTo({
+            behavior: "smooth",
+            top: 4370
+        })
+    })
+}
+
+
